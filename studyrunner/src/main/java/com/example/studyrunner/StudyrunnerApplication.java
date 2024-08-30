931f0934a1b8e232a4248116190bd429deee2d9e
studyrunner <99476223+studyrunner@users.noreply.github.com>
2024-08-30 10:36:03 +0800

    Add files via upload

---
+++ studyrunner/src/main/java/com/example/studyrunner/StudyrunnerApplication.java
@@ -0,0 +1,14 @@
+package com.example.studyrunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyrunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyrunnerApplication.class, args);
+
+	}
+
+}
